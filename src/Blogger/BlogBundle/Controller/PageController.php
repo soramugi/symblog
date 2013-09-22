2768d32d4cd7a2befad43b49a4bb52443d5ecfb7
soramugi <soramugi.chika@gmail.com>
2013-09-22 16:42:01 +0900

    sidebarの機能追加

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -13,10 +13,10 @@ class PageController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()
-                   ->getEntityManager();
+            ->getEntityManager();
 
         $blogs = $em->getRepository('BloggerBlogBundle:Blog')
-                    ->getLatestBlogs();
+            ->getLatestBlogs();
 
         return $this->render('BloggerBlogBundle:Page:index.html.twig', array(
             'blogs' => $blogs
@@ -60,5 +60,27 @@ class PageController extends Controller
             'form' => $form->createView()
         ));
 
+    }
+
+    public function sidebarAction()
+    {
+        $em = $this->getDoctrine()
+            ->getEntityManager();
+
+        $tags = $em->getRepository('BloggerBlogBundle:Blog')
+            ->getTags();
+
+        $tagWeights = $em->getRepository('BloggerBlogBundle:Blog')
+            ->getTagWeights($tags);
+
+        $commentLimit   = $this->container
+            ->getParameter('blogger_blog.comments.latest_comment_limit');
+        $latestComments = $em->getRepository('BloggerBlogBundle:Comment')
+            ->getLatestComments($commentLimit);
+
+        return $this->render('BloggerBlogBundle:Page:sidebar.html.twig', array(
+            'latestComments'    => $latestComments,
+            'tags' => $tagWeights
+        ));
     }
 }
